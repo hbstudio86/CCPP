b759cfe93a48244431ee680dd2e1d2bac3e3fc7c
HBSTUDIO <hbstudio@naver.com>
2022-09-13 22:40:01 +0900

    C++기초플러스 chapter4 프로그래밍 3번 문제 진행중

--- 000000_220913_C4_Programing.cpp
+++ 000000_220913_C4_Programing.cpp
@@ -22,13 +22,18 @@ int main(void) {
 	//cout << "나이: " << Age << endl;
 	////후기 : getline과 get은 한 행을 읽는다. 다만 getline은 개행을 버리는 반면, get은 모두 읽어 들인다.
 	
-	//Q2.string으로 대체해서 4.4 예제 다시 작성 할 것
-	string name, dessert;
-	cout << "이름을 입력하십시오:";
-	getline(cin, name);
-	cout << "좋아하는 디저트를 입력하십시오:";
-	getline(cin, dessert);
-	cout << "맛있는 " << dessert;
-	cout << " 디저트를 준비하겠습니다. " << name << " 님!\n";
+	////Q2.string으로 대체해서 4.4 예제 다시 작성 할 것
+	//string name, dessert;
+	//cout << "이름을 입력하십시오:";
+	//getline(cin, name);
+	//cout << "좋아하는 디저트를 입력하십시오:";
+	//getline(cin, dessert);
+	////cin.getline();
+	//cout << "맛있는 " << dessert;
+	//cout << " 디저트를 준비하겠습니다. " << name << " 님!\n";
+	////후기 string을 한 행으로 읽을 때에는 getline(cin,string)을 써야 한다.
+	////여기서의 getline은 iostream에서의 메서드가 아니다라는 사실을 알 수있다
+
+	//Q3
 	return 0;
 }
