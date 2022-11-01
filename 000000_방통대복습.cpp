321fa76091edd1ff77949f966daa9c012ba9f98f
HBSTUDIO <hbstudio@naver.com>
2022-11-01 22:26:41 +0900

    대학C++ 클래스 상속 연습

--- 000000_방통대복습.cpp
+++ 000000_방통대복습.cpp
@@ -3,13 +3,66 @@
 
 using namespace std;
 
+class A {
+private:
+	int a1;
+protected:
+	int a2;
+public:
+	A() :a1(1), a2(2) {
+		cout << "A 积己磊 积己" << endl;
+	}
+	A(int x) : a1(x), a2(2) {
+		cout << "A 积己磊 积己" << endl;
+	}
+	~A() {
+		cout << "A 颇鲍磊 颇鲍" << endl;
+	}
+	void printA() {
+		cout << a1 << endl;
+	}
+};
+
+class B : public A {
+private:
+	int b1;
+protected:
+	int b2;
+public:
+	B() :b1(3), b2(4) {
+		cout << "B 积己磊 积己" << endl;
+	}
+	B(int x, int y) :b2(x), A(y), b1(3) {
+		cout << "B 积己磊 积己" << endl;
+	}
+	~B() {
+		cout << "B 颇鲍磊 颇鲍" << endl;
+	}
+	void printB() {
+		cout << b1 << endl;
+	}
+	void printALL() {
+		A::printA();
+		cout << b2 << endl;
+	}
+};
+
 int main(void) {
-	int ar[3] = { 1,2,3 };
-	Myclass mc1(3, ar);
-	Myclass mc2(mc1);
-	mc1.print();
-	cout << endl;
-	mc2.print();
-	cout << endl;
+	//int ar[3] = { 1,2,3 };
+	//Myclass mc1(3, ar);
+	//Myclass mc2(mc1);
+	//mc1.print();
+	//cout << endl;
+	//mc2.print();
+	//cout << endl;
+	//return 0;
+
+	//惑加 楷嚼
+	B objB;
+	objB.printB();
+	objB.printALL();
+	B objC(10,20);
+	objC.printB();
+	objC.printALL();
 	return 0;
 }
