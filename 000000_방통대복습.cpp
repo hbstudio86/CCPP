8abfe1f5454d6adb1a08af7a95a280dd6cca0d26
HBSTUDIO <hbstudio@naver.com>
2022-11-08 22:53:21 +0900

    방통대 연습

--- 000000_방통대복습.cpp
+++ 000000_방통대복습.cpp
@@ -6,17 +6,24 @@ using namespace std;
 class A {
 private:
 	int a1;
+	static int totalA;
 protected:
 	int a2;
 public:
 	A() :a1(1), a2(2) {
 		cout << "A 생성자 생성" << endl;
+		totalA++;
+		cout << "totalA : " << totalA << endl;
 	}
 	A(int x) : a1(x), a2(2) {
 		cout << "A 생성자 생성" << endl;
+		totalA++;
+		cout << "totalA : " << totalA << endl;
 	}
 	~A() {
 		cout << "A 파괴자 파괴" << endl;
+		totalA--;
+		cout << "totalA : " << totalA << endl;
 	}
 	void printA() {
 		cout << a1 << endl;
@@ -26,14 +33,19 @@ public:
 class B : public A {
 private:
 	int b1;
+	static int totalB;
 protected:
 	int b2;
 public:
 	B() :b1(3), b2(4) {
 		cout << "B 생성자 생성" << endl;
+		totalB++;
+		cout << "totalB : " << totalB << endl;
 	}
 	B(int x, int y) :b2(x), A(y), b1(3) {
 		cout << "B 생성자 생성" << endl;
+		totalB++;
+		cout << "totalB : " << totalB << endl;
 	}
 	B(const B& b):A(0) {
 		b1 = b.b1;
@@ -41,6 +53,8 @@ public:
 	}
 	~B() {
 		cout << "B 파괴자 파괴" << endl;
+		totalB--;
+		cout << "totalB : " << totalB << endl;
 	}
 	void printB() {
 		cout << b1 << endl;
@@ -49,20 +63,23 @@ public:
 		A::printA();
 		cout << b2 << endl;
 	}
-	B operator++(int) {	//후위연산자
+	B operator++(int) {	//후위연산자 : 뒤에 있어서 (후위) 뒤 (괄호)가 채워져 있다.
 		B tmp(*this);
 		b1++;
 		return tmp;
 	}
-	B& operator++() {	//전위연산자
+	B& operator++() {	//전위연산자 : 앞에 있어서 (전위) 뒤 (괄호)가 비어있다.
 		b1++;
 		return *this;
 	}
 	friend ostream& operator<<(ostream& os, const B& b);
 };
 
-ostream& operator<<(ostream & os,const B& b) {
-	os << b.b1;
+int A::totalA = 0;
+int B::totalB = 0;
+
+ostream& operator<<(ostream & os,const B& b) {	// os << b의 형태로 된다.
+	os << b.b1;	//여기서 os는 cout을 받는다.
 	return os;
 }
 
