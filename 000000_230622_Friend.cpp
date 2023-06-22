f185a4b2f824f61027c69fdfd9db13dd6a20c0e6
HBSTUDIO <hbstudio@naver.com>
2023-06-22 22:29:03 +0900

    Friend 연습

--- 000000_230622_Friend.cpp
+++ 000000_230622_Friend.cpp
@@ -4,15 +4,25 @@ class CSuper {
 private:
 	int n;
 public:
+	CSuper() {
+		n = 0;
+	}
 	friend void fNum(void);
+	void fName() {
+		std::cout << "number is " << this->n;
+	}
 };
 
-void CSuper::fNum(void) {
+void fNum(void) {
 	using namespace std;
-	cout << n;
+	//cout << n;
+	cout << "this is friend!!";
 }
 
 int main() {
 
+	CSuper csa = CSuper();
+	fNum();
+	csa.fName();
 	return 0;
 }
